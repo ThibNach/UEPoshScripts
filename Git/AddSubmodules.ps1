e94f6f1fcff598e3696a62eb8f41304f3a1511de
ThibNach <thibaut.spreux@gmail.com>
2025-04-06 17:18:11 +0200

    add submodule poiters to private forked repos

--- Git/AddSubmodules.ps1
+++ Git/AddSubmodules.ps1
@@ -1,16 +1,17 @@
 # Declare a hashtable with submodule URLs as keys and submodule names as values
 $submodules = @{
-    "git@github.com:TheEmidee/UECommonGame.git" = "Plugins/CommonGame";
-    "git@github.com:TheEmidee/UECommonLoadingScreen.git" = "Plugins/CommonLoadingScreen";
-    "git@github.com:TheEmidee/UECommonUser.git" = "Plugins/CommonUser";
-    "git@github.com:TheEmidee/UECoreExtensions.git" = "Plugins/CoreExtensions";
-    "git@github.com:TheEmidee/UEDataValidationExtensions.git" = "Plugins/DataValidationExtensions";
-    "git@github.com:TheEmidee/UEGameBaseFramework.git" = "Plugins/GameBaseFramework";
-    "git@github.com:TheEmidee/UEGameSettings.git" = "Plugins/GameSettings";
-    "git@github.com:TheEmidee/UEMapCheckvalidation.git" = "Plugins/MapCheckvalidation";
-    "git@github.com:TheEmidee/UEModularGameplayActors.git" = "Plugins/ModularGameplayActors";
-    "git@github.com:TheEmidee/UENamingConventionValidation.git" = "Plugins/NamingConventionValidation";
-    "git@github.com:TheEmidee/UEUIExtension.git" = "Plugins/UIExtension";
+    "git@github.com:ThibNach/UECommonGame.git" = "Plugins/CommonGame";
+    "git@github.com:ThibNach/UECommonLoadingScreen.git" = "Plugins/CommonLoadingScreen";
+    "git@github.com:ThibNach/UECommonUser.git" = "Plugins/CommonUser";
+    "git@github.com:ThibNach/UECoreExtensions.git" = "Plugins/CoreExtensions";
+    "git@github.com:ThibNach/UEDataValidationExtensions.git" = "Plugins/DataValidationExtensions";
+    "git@github.com:ThibNach/UEGameBaseFramework.git" = "Plugins/GameBaseFramework";
+    "git@github.com:ThibNach/UEGameSettings.git" = "Plugins/GameSettings";
+    "git@github.com:ThibNach/UEMapCheckvalidation.git" = "Plugins/MapCheckvalidation";
+    "git@github.com:ThibNach/UEModularGameplayActors.git" = "Plugins/ModularGameplayActors";
+    "git@github.com:ThibNach/UENamingConventionValidation.git" = "Plugins/NamingConventionValidation";
+    "git@github.com:ThibNach/UEUIExtension.git" = "Plugins/UIExtension";
+    "git@github.com:ThibNach/UEGameplayInteractionsExtensions.git" = "Plugins/GameplayInteractionsExtensions"
     
     "git@github.com:ProjectBorealis/UEGitPlugin" = "Plugins/GitPlugin";
 }
