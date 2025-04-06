2959986b732480164083ee82a2130592266ce6cc
ThibNach <thibaut.spreux@gmail.com>
2025-04-06 17:39:06 +0200

    use https submodules addresses

--- Git/AddSubmodules.ps1
+++ Git/AddSubmodules.ps1
@@ -1,19 +1,19 @@
 # Declare a hashtable with submodule URLs as keys and submodule names as values
 $submodules = @{
-    "git@github.com:ThibNach/UECommonGame.git" = "Plugins/CommonGame";
-    "git@github.com:ThibNach/UECommonLoadingScreen.git" = "Plugins/CommonLoadingScreen";
-    "git@github.com:ThibNach/UECommonUser.git" = "Plugins/CommonUser";
-    "git@github.com:ThibNach/UECoreExtensions.git" = "Plugins/CoreExtensions";
-    "git@github.com:ThibNach/UEDataValidationExtensions.git" = "Plugins/DataValidationExtensions";
-    "git@github.com:ThibNach/UEGameBaseFramework.git" = "Plugins/GameBaseFramework";
-    "git@github.com:ThibNach/UEGameSettings.git" = "Plugins/GameSettings";
-    "git@github.com:ThibNach/UEMapCheckvalidation.git" = "Plugins/MapCheckvalidation";
-    "git@github.com:ThibNach/UEModularGameplayActors.git" = "Plugins/ModularGameplayActors";
-    "git@github.com:ThibNach/UENamingConventionValidation.git" = "Plugins/NamingConventionValidation";
-    "git@github.com:ThibNach/UEUIExtension.git" = "Plugins/UIExtension";
-    "git@github.com:ThibNach/UEGameplayInteractionsExtensions.git" = "Plugins/GameplayInteractionsExtensions"
+    "https://github.com/ThibNach/UECommonGame.git" = "Plugins/CommonGame";
+    "https://github.com/ThibNach/UECommonLoadingScreen.git" = "Plugins/CommonLoadingScreen";
+    "https://github.com/ThibNach/UECommonUser.git" = "Plugins/CommonUser";
+    "https://github.com/ThibNach/UECoreExtensions.git" = "Plugins/CoreExtensions";
+    "https://github.com/ThibNach/UEDataValidationExtensions.git" = "Plugins/DataValidationExtensions";
+    "https://github.com/ThibNach/UEGameBaseFramework.git" = "Plugins/GameBaseFramework";
+    "https://github.com/ThibNach/UEGameSettings.git" = "Plugins/GameSettings";
+    "https://github.com/ThibNach/UEMapCheckvalidation.git" = "Plugins/MapCheckvalidation";
+    "https://github.com/ThibNach/UEModularGameplayActors.git" = "Plugins/ModularGameplayActors";
+    "https://github.com/ThibNach/UENamingConventionValidation.git" = "Plugins/NamingConventionValidation";
+    "https://github.com/ThibNach/UEUIExtension.git" = "Plugins/UIExtension";
+    "https://github.com/ThibNach/UEGameplayInteractionsExtensions.git" = "Plugins/GameplayInteractionsExtensions"
     
-    "git@github.com:ProjectBorealis/UEGitPlugin" = "Plugins/GitPlugin";
+    # "git@github.com:ProjectBorealis/UEGitPlugin" = "Plugins/GitPlugin";
 }
 
 # Path to the Git repository folder where you want to add the submodules
